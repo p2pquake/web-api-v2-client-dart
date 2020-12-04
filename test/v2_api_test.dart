321737725d35c695bf40d165e4f657d0dcd4822e
typewriter <takuyaxp@gmail.com>
2020-12-04 16:39:06 +0900

    Add JMAQuake tests

--- test/v2_api_test.dart
+++ test/v2_api_test.dart
@@ -51,7 +51,121 @@ void main() {
         expect(v.length, equals(2));
       });
 
-      test('returns JMAQuakes', () async {});
+      test('returns JMAQuakes', () async {
+        replaceResponse(
+          '[' +
+              // ScalePrompt
+              '{"code":551,"earthquake":{"domesticTsunami":"Checking","foreignTsunami":"Unknown","hypocenter":{"depth":-1,"latitude":-200,"longitude":-200,"magnitude":-1,"name":""},"maxScale":70,"time":"2016/04/14 21:26:00"},"id":"570f976e843b7327f4000000","issue":{"correct":"None","source":"気象庁","time":"2016/04/14 21:29:00","type":"ScalePrompt"},"points":[{"addr":"熊本県熊本","isArea":true,"pref":"熊本県","scale":70},{"addr":"熊本県阿蘇","isArea":true,"pref":"熊本県","scale":45},{"addr":"鹿児島県甑島","isArea":true,"pref":"鹿児島県","scale":30}],"time":"2016/04/14 21:29:00","user-agent":"quake_checker/analyzer 20130830","ver":"20140906"},' +
+              // Destination
+              '{"code":551,"created_at":"2020/12/01 08:02:10.108","earthquake":{"domesticTsunami":"None","foreignTsunami":"Unknown","hypocenter":{"depth":610,"latitude":48.1,"longitude":141.1,"magnitude":6.6,"name":"サハリン西方沖"},"maxScale":-1,"time":"2020/12/01 07:55:00"},"id":"5fc579f202add60b326981f3","issue":{"correct":"None","source":"気象庁","time":"2020/12/01 07:59:00","type":"Destination"},"points":[],"time":"2020/12/01 08:01:06.587","user-agent":"quake_checker/analyzer 20180128","ver":"20180128"},' +
+              // ScaleAndDestination (津波予報なし, 観測点なし)
+              '{"code":551,"created_at":"2020/12/04 00:25:33.733","earthquake":{"domesticTsunami":"None","foreignTsunami":"Unknown","hypocenter":{"depth":10,"latitude":33.8,"longitude":139.3,"magnitude":2.9,"name":"三宅島近海"},"maxScale":10,"time":"2020/12/04 00:20:00"},"id":"5fc9036d02add60b3269852d","issue":{"correct":"None","source":"気象庁","time":"2020/12/04 00:23:00","type":"ScaleAndDestination"},"points":[],"time":"2020/12/04 00:25:08.636","user-agent":"quake_checker/analyzer 20180128","ver":"20180128"},' +
+              // ScaleAndDestination (津波予報あり)
+              '{"code":551,"created_at":"2019/06/18 22:28:42.873","earthquake":{"domesticTsunami":"Warning","foreignTsunami":"Unknown","hypocenter":{"depth":10,"latitude":38.6,"longitude":139.5,"magnitude":6.8,"name":"山形県沖"},"maxScale":60,"time":"2019/06/18 22:22:00"},"id":"5d08e70a02add629efecb61f","issue":{"correct":"None","source":"気象庁","time":"2019/06/18 22:26:00","type":"ScaleAndDestination"},"points":[{"addr":"新潟県下越","isArea":true,"pref":"新潟県","scale":60},{"addr":"山形県庄内","isArea":true,"pref":"山形県","scale":55},{"addr":"岩手県内陸北部","isArea":true,"pref":"岩手県","scale":40}],"time":"2019/06/18 22:28:08.209","user-agent":"quake_checker/analyzer 20180128","ver":"20180128"},' +
+              // DetailScale
+              '{"code":551,"created_at":"2018/09/06 03:15:06.880","earthquake":{"domesticTsunami":"NonEffective","foreignTsunami":"Unknown","hypocenter":{"depth":40,"latitude":42.7,"longitude":142,"magnitude":6.7,"name":"胆振地方中東部"},"maxScale":60,"time":"2018/09/06 03:08:00"},"id":"5b901d2af837cf68164a675f","issue":{"correct":"None","source":"気象庁","time":"2018/09/06 03:12:00","type":"DetailScale"},"points":[{"addr":"安平町早来北進","isArea":false,"pref":"北海道","scale":60},{"addr":"札幌北区太平","isArea":false,"pref":"北海道","scale":50},{"addr":"厚真町鹿沼","isArea":false,"pref":"北海道","scale":46},{"addr":"平内町東田沢","isArea":false,"pref":"青森県","scale":20},{"addr":"村上市岩船駅前","isArea":false,"pref":"新潟県","scale":10}],"time":"2018/09/06 03:14:05.261","user-agent":"quake_checker/analyzer 20180128","ver":"20180128"},' +
+              // Foreign
+              '{"code":551,"created_at":"2020/10/20 06:32:36.648","earthquake":{"domesticTsunami":"None","foreignTsunami":"WarningNearby","hypocenter":{"depth":-1,"latitude":54.7,"longitude":-159.6,"magnitude":7.4,"name":"アラスカ州南方"},"maxScale":-1,"time":"2020/10/20 05:55:00"},"id":"5f8e05f402add60b34694906","issue":{"correct":"None","source":"気象庁","time":"2020/10/20 06:30:00","type":"Foreign"},"points":[],"time":"2020/10/20 06:32:05.903","user-agent":"quake_checker/analyzer 20180128","ver":"20180128"},' +
+              // Other
+              '{"code":551,"created_at":"2020/11/22 21:03:07.981","earthquake":{"domesticTsunami":"Unknown","foreignTsunami":"Unknown","hypocenter":{"depth":-1,"latitude":-200,"longitude":-200,"magnitude":-1,"name":""},"maxScale":-1,"time":"0001/01/01 00:00:00"},"id":"5fba537b02add60b2f696cbf","issue":{"correct":"Unknown","source":"気象庁","time":"2020/11/22 21:00:00","type":"Other"},"points":[],"time":"2020/11/22 21:02:06.064","user-agent":"quake_checker/analyzer 20180128","ver":"20180128"}' +
+              ']',
+        );
+        final v = await instance.historyGet();
+        v.forEach((element) {
+          expect(element, isA<JMAQuake>());
+        });
+
+        final quakes = v.map((e) => e as JMAQuake).toList();
+
+        final scalePrompt = quakes[0];
+        expect(scalePrompt.issue.type, equals(JMAQuakeAllOfIssueTypeEnum.scalePrompt));
+        expect(scalePrompt.earthquake.time, equals("2016/04/14 21:26:00"));
+        expect(
+            scalePrompt.earthquake.maxScale, equals(JMAQuakeAllOfEarthquakeMaxScaleEnum.scale70));
+        expect(scalePrompt.earthquake.domesticTsunami,
+            equals(JMAQuakeAllOfEarthquakeDomesticTsunamiEnum.checking));
+        expect(scalePrompt.points.length, equals(3));
+        expect(scalePrompt.points[0].addr, equals("熊本県熊本"));
+        expect(scalePrompt.points[0].isArea, isTrue);
+        expect(scalePrompt.points[0].pref, equals("熊本県"));
+        expect(scalePrompt.points[0].scale, equals(JMAQuakeAllOfPointsScaleEnum.scale70));
+
+        final destination = quakes[1];
+        expect(destination.issue.type, equals(JMAQuakeAllOfIssueTypeEnum.destination));
+        expect(destination.earthquake.time, equals("2020/12/01 07:55:00"));
+        expect(destination.earthquake.domesticTsunami,
+            equals(JMAQuakeAllOfEarthquakeDomesticTsunamiEnum.none));
+        expect(destination.earthquake.hypocenter.depth, equals(610));
+        expect(destination.earthquake.hypocenter.latitude, equals(48.1));
+        expect(destination.earthquake.hypocenter.longitude, equals(141.1));
+        expect(destination.earthquake.hypocenter.magnitude, equals(6.6));
+        expect(destination.earthquake.hypocenter.name, equals("サハリン西方沖"));
+
+        final scaleAndDest1 = quakes[2];
+        expect(scaleAndDest1.issue.type, equals(JMAQuakeAllOfIssueTypeEnum.scaleAndDestination));
+        expect(scaleAndDest1.earthquake.time, equals("2020/12/04 00:20:00"));
+        expect(
+            scaleAndDest1.earthquake.maxScale, equals(JMAQuakeAllOfEarthquakeMaxScaleEnum.scale10));
+        expect(scaleAndDest1.earthquake.domesticTsunami,
+            equals(JMAQuakeAllOfEarthquakeDomesticTsunamiEnum.none));
+        expect(scaleAndDest1.earthquake.hypocenter.depth, equals(10));
+        expect(scaleAndDest1.earthquake.hypocenter.latitude, equals(33.8));
+        expect(scaleAndDest1.earthquake.hypocenter.longitude, equals(139.3));
+        expect(scaleAndDest1.earthquake.hypocenter.magnitude, equals(2.9));
+        expect(scaleAndDest1.earthquake.hypocenter.name, equals("三宅島近海"));
+        expect(scaleAndDest1.points, isEmpty);
+
+        final scaleAndDest2 = quakes[3];
+        expect(scaleAndDest2.issue.type, equals(JMAQuakeAllOfIssueTypeEnum.scaleAndDestination));
+        expect(scaleAndDest2.earthquake.time, equals("2019/06/18 22:22:00"));
+        expect(
+            scaleAndDest2.earthquake.maxScale, equals(JMAQuakeAllOfEarthquakeMaxScaleEnum.scale60));
+        expect(scaleAndDest2.earthquake.domesticTsunami,
+            equals(JMAQuakeAllOfEarthquakeDomesticTsunamiEnum.warning));
+        expect(scaleAndDest2.earthquake.hypocenter.depth, equals(10));
+        expect(scaleAndDest2.earthquake.hypocenter.latitude, equals(38.6));
+        expect(scaleAndDest2.earthquake.hypocenter.longitude, equals(139.5));
+        expect(scaleAndDest2.earthquake.hypocenter.magnitude, equals(6.8));
+        expect(scaleAndDest2.earthquake.hypocenter.name, equals("山形県沖"));
+        expect(scaleAndDest2.points.length, equals(3));
+        expect(scaleAndDest2.points[0].addr, equals("新潟県下越"));
+        expect(scaleAndDest2.points[0].scale, equals(JMAQuakeAllOfPointsScaleEnum.scale60));
+
+        final detail = quakes[4];
+        expect(detail.issue.type, equals(JMAQuakeAllOfIssueTypeEnum.detailScale));
+        expect(detail.earthquake.time, equals("2018/09/06 03:08:00"));
+        expect(detail.earthquake.maxScale, equals(JMAQuakeAllOfEarthquakeMaxScaleEnum.scale60));
+        expect(detail.earthquake.domesticTsunami,
+            equals(JMAQuakeAllOfEarthquakeDomesticTsunamiEnum.nonEffective));
+        expect(detail.earthquake.hypocenter.depth, equals(40));
+        expect(detail.earthquake.hypocenter.latitude, equals(42.7));
+        expect(detail.earthquake.hypocenter.longitude, equals(142));
+        expect(detail.earthquake.hypocenter.magnitude, equals(6.7));
+        expect(detail.earthquake.hypocenter.name, equals("胆振地方中東部"));
+        expect(detail.points.length, equals(5));
+        expect(detail.points[2].addr, equals("厚真町鹿沼"));
+        expect(detail.points[2].isArea, isFalse);
+        expect(detail.points[2].pref, equals("北海道"));
+        expect(detail.points[2].scale, equals(JMAQuakeAllOfPointsScaleEnum.scale46));
+
+        final foreign = quakes[5];
+        expect(foreign.issue.type, equals(JMAQuakeAllOfIssueTypeEnum.foreign));
+        expect(foreign.earthquake.time, equals("2020/10/20 05:55:00"));
+        expect(foreign.earthquake.domesticTsunami,
+            equals(JMAQuakeAllOfEarthquakeDomesticTsunamiEnum.none));
+        expect(foreign.earthquake.foreignTsunami,
+            equals(JMAQuakeAllOfEarthquakeForeignTsunamiEnum.warningNearby));
+        expect(foreign.earthquake.hypocenter.depth, equals(-1));
+        expect(foreign.earthquake.hypocenter.latitude, equals(54.7));
+        expect(foreign.earthquake.hypocenter.longitude, equals(-159.6));
+        expect(foreign.earthquake.hypocenter.magnitude, equals(7.4));
+        expect(foreign.earthquake.hypocenter.name, equals("アラスカ州南方"));
+        expect(foreign.points, isEmpty);
+
+        final other = quakes[6];
+        expect(other.issue.type, equals(JMAQuakeAllOfIssueTypeEnum.other));
+      });
+
       test('returns JMATsunamis', () async {
         replaceResponse(
             '[{"areas":[{"grade":"Warning","immediate":false,"name":"福島県"},{"grade":"Watch","immediate":false,"name":"青森県太平洋沿岸"},{"grade":"Watch","immediate":false,"name":"岩手県"},{"grade":"Watch","immediate":false,"name":"宮城県"},{"grade":"Watch","immediate":false,"name":"茨城県"},{"grade":"Watch","immediate":false,"name":"千葉県九十九里・外房"},{"grade":"Watch","immediate":true,"name":"千葉県内房"},{"grade":"Watch","immediate":true,"name":"伊豆諸島"}],"cancelled":false,"code":552,"id":"5d615d7cb94edb2ab8d60a0e","issue":{"source":"気象庁","time":"2016/11/22 07:26:00","type":"Focus"},"time":"2016/11/22 07:29:00","user-agent":"tsunami_checker/analyzer 20180128","ver":"20180128"},' +
